e6f301d248819d0b27f23de4c502fa282949b3fd
Emircan ERKUL <develop@emircanerkul.com>
2021-10-02 12:41:19 +0300

    security

--- pwa/pages/admin/index.tsx
+++ pwa/pages/admin/index.tsx
@@ -1,21 +1,60 @@
 import Head from "next/head";
+import React from "react";
 
-const AdminLoader = () => {
-  if (typeof window !== "undefined") {
-    const { HydraAdmin } = require("@api-platform/admin");
-    return <HydraAdmin entrypoint={window.origin} />;
+import { ENTRYPOINT } from "config/entrypoint";
+
+import { Redirect, Route } from "react-router-dom";
+import { HydraAdmin, hydraDataProvider as baseHydraDataProvider, fetchHydra as baseFetchHydra, useIntrospection } from "@api-platform/admin";
+import parseHydraDocumentation from "@api-platform/api-doc-parser/lib/hydra/parseHydraDocumentation";
+import authProvider from "utils/authProvider";
+import Login from "components/admin/login";
+
+const getHeaders = () => localStorage.getItem("token") ? {
+  Authorization: `Bearer ${localStorage.getItem("token")}`,
+} : {};
+const fetchHydra = (url, options = {}) =>
+  baseFetchHydra(url, {
+    ...options,
+    headers: getHeaders,
+  });
+const RedirectToLogin = () => {
+  const introspect = useIntrospection();
+
+  if (localStorage.getItem("token")) {
+    introspect();
+    return <></>;
   }
-
-  return <></>;
+  return <Redirect to="/login" />;
 };
+const apiDocumentationParser = async (ENTRYPOINT) => {
+  try {
+    const { api } = await parseHydraDocumentation(ENTRYPOINT, { headers: getHeaders });
+    return { api };
+  } catch (result) {
+    if (result.status === 401) {
+      // Prevent infinite loop if the token is expired
+      localStorage.removeItem("token");
+
+      return {
+        api: result.api,
+        customRoutes: [
+          <Route path="/" component={RedirectToLogin} />
+        ],
+      };
+    }
+
+    throw result;
+  }
+};
+const dataProvider = baseHydraDataProvider(ENTRYPOINT, fetchHydra, apiDocumentationParser);
 
 const Admin = () => (
-  <>
-    <Head>
-      <title>API Platform Admin</title>
-    </Head>
-
-    <AdminLoader />
-  </>
+  <HydraAdmin
+    dataProvider={ dataProvider }
+    authProvider={ authProvider }
+    entrypoint={ ENTRYPOINT }
+    loginPage={Login}
+  />
 );
+
 export default Admin;
